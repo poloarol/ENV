589a69639f0d3f523ccb0a46b3e1e83a937961c7
Polo Arol <paularol211@yahoo.com>
2018-02-16 22:26:04 -0500

    Fixed screen display, drawing would be produced on same scale for each device

--- static/scripts/draw.js
+++ static/scripts/draw.js
@@ -1,22 +1,31 @@
 function draw(parsedData){
-  var svgContainer = d3.select(".svg").append("svg")
-              .attr("height", "100%")
-              .attr("width", "100%");
+  var margin = {top: 20, right: 100, bottom: 20, left: 100};
 
-  console.log(parsedData)
+  width = document.documentElement.clientWidth;
+
+  var width = width - margin.left - margin.right;
+  var height = 200 - margin.top - margin.bottom;
+
+  var svgContainer = d3.select(".svg").append("svg")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                        .attr("style", "outline: thin solid red")
+                        .attr("viewbox", "0 0 1500 200")
+                        .attr("preserveAspectRatio","xMidYMid meet");
 
   parsedData.sort(function(a, b){
     return a.Start - b.Start
   });
 
-
   // Define the x-axis horizontal
   var minValue = parsedData[0].Start;
-  var maxValue = 1000 + parsedData[parsedData.length - 1].Stop;
+  var maxValue = 5000 + parsedData[parsedData.length - 1].Stop;
 
-  var xScale = d3.scaleLinear()
+  xScale = d3.scaleLinear()
                        .domain([minValue, maxValue])
-                       .range([50, 1000])
+                       .range([100, width]);
 
   var line = svgContainer.selectAll("line")
                 .data(parsedData)
@@ -25,10 +34,10 @@ function draw(parsedData){
                   if(i== 0){
                     return xScale(d.Start)
                   }
-                  return xScale(100 + d.Start)})
-                .attr("y1", "100")
-                .attr("x2", function(d){ return xScale(d.Stop) })
-                .attr("y2", "100")
+                  return xScale(d.Start)})
+                .attr("y1", "50")
+                .attr("x2", function(d){ return xScale(d.Stop)})
+                .attr("y2", "50")
                 .attr("stroke", function(d){
                   if(d.Strand === 1){
                     return "#291B2C";
@@ -44,39 +53,14 @@ function draw(parsedData){
                   return "Gene: " + d.Gene + "\nLocus Tag: " + d.LocusTag + "\nProduct: " + d.Product;
                 })
 
-
-  // tableCreate(parsedData);
-
   }
 
-  function tableCreate(parsedData){
+  d3.select(window).on('resize', resize);
 
-    var myArray = ["Gene", "Locus tag", "Product"]
+  function resize(){
+    // update width
+    width = document.documentElement.clientWidth;
 
-    var tableDiv = document.getElementById('table');
-    var tbl = document.createElement('table');
-    tbl.style.width = "100%";
-    tbl.style.border = "1px solid black";
-    // var tRow = tbl.insertRow();
-    //
-    // for (var i = 0; i < myArray.length; i++) {
-    //   var tHead = tRow.insertCell();
-    //   tHead.appendChild(document.createTextNode(myArray[i]));
-    // }
+    // reset x range
 
-    for (var i = 0; i < parsedData.length; i++) {
-      var tr = tbl.insertRow();
-      for (var i = 0; i < myArray.length; i++) {
-        var th = tr.insertCell();
-        if(i == 0){
-          th.appendChild(document.createTextNode(parsedData[i].Gene));
-        }else if(i == 1){
-          th.appendChild(document.createTextNode(parsedData[i].LocusTag));
-        }else{
-          th.appendChild(document.createTextNode(parsedData[i].Product))
-        }
-      }
-    }
-
-  tableDiv.appendChild(tbl);
   }
